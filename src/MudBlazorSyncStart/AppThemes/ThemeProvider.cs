f543a48f841a3d52d6bb4b13694d65ec0b93215a
Sergiusz Kriwobokow <writesd@hotmail.com>
2022-11-30 09:06:37 +0100

    no comments

--- src/MudBlazorSyncStart/AppThemes/ThemeProvider.cs
+++ src/MudBlazorSyncStart/AppThemes/ThemeProvider.cs
@@ -4,14 +4,51 @@ namespace MudBlazorSyncStart.AppThemes
 {
     public interface IThemeProvider
     {
-        MudTheme FirstTheme { get; }
+        MudTheme ThemeA { get; }
+        MudTheme ThemeB { get; }
+
     }
 
     public class ThemeProvider : IThemeProvider
     {
-        //private MudTheme? theme;
-
-        public MudTheme FirstTheme
+        public MudTheme ThemeA
+        {
+            get
+            {
+                var theme = new MudTheme()
+                {
+                    Palette = new Palette()
+                    {
+                        Primary = Colors.Blue.Default,
+                        PrimaryLighten= Colors.Blue.Lighten2,
+                        PrimaryDarken= Colors.Blue.Darken2,
+                        
+                        Secondary = Colors.Green.Accent4,
+                        AppbarBackground = Colors.Purple.Accent4,
+                        AppbarText = Colors.Shades.White,
+                        DrawerBackground = Colors.DeepPurple.Lighten1,
+                        DrawerText = Colors.Shades.White,
+                        
+                    },
+                    PaletteDark = new PaletteDark()
+                    {
+                        Primary = Colors.Blue.Lighten1,
+                        DrawerText = Colors.Grey.Default,
+                        AppbarText = Colors.Grey.Default,
+                    },
+                    LayoutProperties = new LayoutProperties()
+                    {
+                        DrawerWidthLeft = "260px",
+                        DrawerWidthRight = "300px"
+                    },
+                    //Shadows = new Shadow() {},
+                    //Typography = new Typography() {},
+                    //ZIndex = new ZIndex() { }
+                };
+                return theme;
+            }
+        }
+        public MudTheme ThemeB
         {
             get
             {
@@ -44,5 +81,6 @@ namespace MudBlazorSyncStart.AppThemes
                 return theme;
             }
         }
+
     }
 }
